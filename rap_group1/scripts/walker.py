af8690c5dba6892a64e9d90bda18c094dc849b77
krenztim <krenztim@students.zhaw.ch>
2021-05-26 16:43:54 +0000

    fix last marker

--- rap_group1/scripts/walker.py
+++ rap_group1/scripts/walker.py
@@ -14,7 +14,7 @@ class Walker:
         self.velocity_publisher.publish(stop_movements)
 
     def spin(self):
-        rospy.loginfo("Start spinning")
+        #rospy.loginfo("Start spinning")
         spin = Twist()
         spin.angular.z = -0.5
         self.velocity_publisher.publish(spin)
